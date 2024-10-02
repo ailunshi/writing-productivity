83b1324909e7bdbadf903900adfabfcf37ca24a2
ailunshi <ailunshi@berkeley.edu>
2024-10-02 12:59:00 -0700

    connected to scrivx_parser and added in word count calculations

--- writing_streamlined.py
+++ writing_streamlined.py
@@ -2,6 +2,7 @@
 #next features: pull word count from file
 
 import datetime, csv, os
+import scrivx_parser
 
 def str_time(time):
         #Returns datetime object as a string
@@ -16,17 +17,18 @@ class WritingSessionTracker:
         #instantiates a session as a list within a list
         #self.sessions = [] #needs to pull existing list from file
         self.session = {}
-        self.file = "new_file.csv"
+        self.file = "Writing Tracker.csv"
+        self.data = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Files/Data"
         self.project_path = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Bel e Kyre.scrivx"
         #self.metadata_path = os.path.join(self.project_path, "Settings", "ui.plist")
-    
+
     def start_session(self):
         if os.path.exists(self.file):
             pass
         else:
             with open(self.file, "a", newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(["Start Time", "End Time", "Total Time"])
+                writer.writerow(["Start Time", "End Time", "Total Time", "Start Count", "End Count", "Words"])
 
         response = input("Type anything to start.")
         if response == "":
@@ -36,6 +38,7 @@ class WritingSessionTracker:
             str_start_time = str_time(start_time)
             print("Start Time: ", str_start_time)
             self.session["start"] = start_time
+            self.session["start_count"] = scrivx_parser.get_total_word_count(self.data, self.project_path)
 
         additional_response = input("Type anything when you're finished with this writing session.")
         if response == "":
@@ -48,6 +51,8 @@ class WritingSessionTracker:
     def end_session(self):
         end_time = datetime.datetime.now()
         self.session["end"] = end_time
+        self.session["end_count"] = scrivx_parser.get_total_word_count(self.data, self.project_path)
+        self.session["words"] = self.session["end_count"] - self.session["start_count"]
         
     def calculation(self):
         self.session["total_time"] = chop_ms(self.session["end"] - self.session["start"])
@@ -56,7 +61,7 @@ class WritingSessionTracker:
     def write_to_file(self):
         with open(self.file, "a", newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow([str_time(self.session["start"]), str_time(self.session["end"]), str(self.session["total_time"])])
+            writer.writerow([str_time(self.session["start"]), str_time(self.session["end"]), str(self.session["total_time"]), self.session["start_count"], self.session["end_count"], self.session["words"]])
 
 tracker = WritingSessionTracker()
 tracker.start_session()
