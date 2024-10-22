6c2cbec7b81382355a773452ef6e264d727640a5
ailunshi <ailunshi@berkeley.edu>
2024-10-22 11:00:24 -0700

    added in JSON file functionality to keep track of all writing sessions for a given project

--- writing_streamlined.py
+++ writing_streamlined.py
@@ -1,27 +1,41 @@
-#writing productivity tracker streamlined into a class with dictionary of sessions
-#next features: pull word count from file
+"""
+To Do:
+• Need to abstract this away. Set up new file that asks for project path and data path
+ and creates a new WritingSessionTracker object with those paths.
+• Add data.csv functionality and make sure info can be pulled from .json (especially datetime info)
+    • ie. do i need to convert datetime info back from iso if i want to calculate things with it?
+"""
 
-import datetime, csv, os, time
+import datetime, csv, os, time, json
 from scrivx_parser import ScrivxParser
 
 def str_time(time):
-        #Returns datetime object as a string
-        return time.strftime("%m/%d/%Y, %H:%M:%S")
+    # Returns datetime object as a string
+    return time.strftime("%m/%d/%Y, %H:%M:%S")
 
 def chop_ms(delta):
-    #Returns timedelta object without microseconds
+    # Returns timedelta object without microseconds
     return delta - datetime.timedelta(microseconds=delta.microseconds)
 
+def serialize_datetime(obj): 
+    if isinstance(obj, datetime.datetime): 
+        return obj.isoformat() 
+    raise TypeError("Type not serializable") 
+
 class WritingSessionTracker:
-    def __init__(self):
+    def __init__(self, filename="sessions.json"):
+        # Sets up a WritingSessionTracker object with sessions dictionary, tracker file, and data file
+        
+        self.jsonfile = filename
+        self.all_sessions = self.load_session()
         self.session = {}
         self.tracker = "writingtracker.csv"
-        self.tracker_data = "Data.csv"
+        self.tracker_data = "data.csv"
         self.data = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Files/Data"
         self.project_path = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Bel e Kyre.scrivx"
 
     def start_session(self):
-        # Starts the session, instantiates a parser, runs parser
+        # Starts the session, initiates a ScrivxParser object, runs parser
 
         parser = ScrivxParser(self.data, self.project_path)
         parser.parse_scrivx_file()
@@ -30,8 +44,11 @@ class WritingSessionTracker:
             pass
         else:
             with open(self.tracker, "a", newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(["Date", "Day", "Start Time", "End Time", "Elapsed Time", "Start Count", "End Count", "Words"])
+            #open(self.tracker, "a", newline='') as datafile:
+                writer1 = csv.writer(csvfile)
+                #writer2 = csv.writer(datafile)
+                writer1.writerow(["Date", "Day", "Start Time", "End Time", "Elapsed Time", "Start Count", "End Count", "Words"])
+                #writer2.writerow(["Average Rate", "Total Words", "Total Time", "Total Sessions", "Average Words Per Session", "Average Time Per Session"])
 
         response = input("Type anything to start.")
         if response == "":
@@ -42,10 +59,6 @@ class WritingSessionTracker:
             self.session["date"] = start_timestamp.strftime("%m/%d/%Y")
             self.session["day"] = start_timestamp.strftime("%A")
             self.session["start_time"] = start_timestamp.strftime("%H:%M:%S")
-
-            #str_start_time = str_time(start_timestamp)
-            #print("Start Time: ", str_start_time)
-            #self.session["start"] = start_timestamp
 
             counter_start = time.perf_counter()
 
@@ -60,7 +73,9 @@ class WritingSessionTracker:
             print("No input was given.")
         else:
             self.end_session(parser)
-            self.write_to_file()
+            self.write_to_raw_file()
+            #self.write_to_data_file()
+            self.save_session()
 
     def end_session(self, parser):
         # Ends session and calculates the data
@@ -70,13 +85,39 @@ class WritingSessionTracker:
         self.session["end_count"] = parser.run()
         self.session["words"] = self.session["end_count"] - self.session["start_count"]
         self.session["elapsed_time"] = str(chop_ms(self.session["end_timestamp"] - self.session["start_timestamp"]))
-        print(self.session)
 
-    def write_to_file(self):
+    def write_to_raw_file(self):
         # Writes all the pertinent data into a CSV file
         with open(self.tracker, "a", newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([self.session["date"], self.session["day"], self.session["start_time"], self.session["end_time"], self.session["elapsed_time"], self.session["start_count"], self.session["end_count"], self.session["words"]])
+
+    #def write_to_data_file(self):
+        # Compiles more useful data based on information from the CSV file
+
+        #"Average Rate", "Total Words", "Total Time", "Total Sessions", "Average Words Per Session", "Average Time Per Session"
+
+        #with open(self.tracker, "a") as datafile:
+            #writer = csv.writer(datafile)
+            #writer.writerow([])
+
+    def load_session(self):
+        # Loads all sessions for this project from a JSON file
+
+        if os.path.exists(self.jsonfile):
+            with open(self.jsonfile, 'r') as f:
+                return json.load(f)
+        else:
+            return []
+
+    def save_session(self):
+        # Saves current writing session to a JSON file
+
+        self.all_sessions.append(self.session)
+        with open("sessions.json", "w") as file:
+            json.dump(self.all_sessions, file, default=serialize_datetime)
+
+        print(file.name)
 
 if __name__== "__main__":
     tracker = WritingSessionTracker()
