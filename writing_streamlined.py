a8a9186ac3bb5cfd4f055caf380c30dbd736e79c
ailunshi <ailunshi@berkeley.edu>
2024-10-28 12:45:50 -0700

    a disaster -- still trying to calculate time

--- writing_streamlined.py
+++ writing_streamlined.py
@@ -22,6 +22,12 @@ def serialize_datetime(obj):
         return obj.isoformat() 
     raise TypeError("Type not serializable") 
 
+def unserialize_datetime(date_string):
+    try:
+        return datetime.fromisoformat(date_string)
+    except ValueError:
+        raise TypeError("Date string not in ISO format")
+
 class WritingSessionTracker:
     def __init__(self, filename="sessions.json"):
         # Sets up a WritingSessionTracker object with sessions dictionary, tracker file, and data file
@@ -44,11 +50,8 @@ class WritingSessionTracker:
             pass
         else:
             with open(self.tracker, "a", newline='') as csvfile:
-            #open(self.tracker, "a", newline='') as datafile:
-                writer1 = csv.writer(csvfile)
-                #writer2 = csv.writer(datafile)
-                writer1.writerow(["Date", "Day", "Start Time", "End Time", "Elapsed Time", "Start Count", "End Count", "Words"])
-                #writer2.writerow(["Average Rate", "Total Words", "Total Time", "Total Sessions", "Average Words Per Session", "Average Time Per Session"])
+                writer = csv.writer(csvfile)
+                writer.writerow(["Date", "Day", "Start Time", "End Time", "Elapsed Time", "Start Count", "End Count", "Words"])
 
         response = input("Type anything to start.")
         if response == "":
@@ -74,7 +77,7 @@ class WritingSessionTracker:
         else:
             self.end_session(parser)
             self.write_to_raw_file()
-            #self.write_to_data_file()
+            self.write_to_data_file()
             self.save_session()
 
     def end_session(self, parser):
@@ -84,13 +87,14 @@ class WritingSessionTracker:
         self.session["end_time"] = end_timestamp.strftime("%H:%M:%S")
         self.session["end_count"] = parser.run()
         self.session["words"] = self.session["end_count"] - self.session["start_count"]
-        self.session["elapsed_time"] = str(chop_ms(self.session["end_timestamp"] - self.session["start_timestamp"]))
+        self.session["elapsed_time_str"] = str(chop_ms(self.session["end_timestamp"] - self.session["start_timestamp"]))
+        self.session["elapsed_time_sec"] = int(chop_ms(self.session["end_timestamp"] - self.session["start_timestamp"]).total_seconds())
 
     def write_to_raw_file(self):
         # Writes all the pertinent data into a CSV file
         with open(self.tracker, "a", newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow([self.session["date"], self.session["day"], self.session["start_time"], self.session["end_time"], self.session["elapsed_time"], 
+            writer.writerow([self.session["date"], self.session["day"], self.session["start_time"], self.session["end_time"], self.session["elapsed_time_str"], 
                              self.session["start_count"], self.session["end_count"], self.session["words"]])
 
     def write_to_data_file(self):
@@ -101,21 +105,47 @@ class WritingSessionTracker:
         # you have a list of all sessions (as dictionaries)
         # for dict in list
         # set up a dictionary with key as date, and value as dictionary of all other information
+
+        #what info needs to be unserialized? elapsed_time, start_timestamp, end_timestamp.
+
         data = {}
 
         for item in self.all_sessions: #item is a dictionary
-            if item["date"] in data.keys():
-                average_rate = (data[item]["total_words"] + item["words"] * 60) / (data[item]["elapsed_time"] + item["elapsed_time"])
-                data[item] = [{"average_rate": average_rate, "total_words": data[item]["total_words"] + item["words"], "total_time": data[item]["elapsed_time"] + item["elapsed_time"],
-                               "total_sessions": data[item]["total_sessions"] + 1, "average_words_per_session": (data[item]["total_words"] + item["words"]) / (data[item]["total_sessions"] + 1),
-                               "average_time_per_session": (data[item]["elapsed_time"] + item["elapsed_time"]) / (data[item]["total_sessions"] + 1)}]
-            else:
-                data[item] = [{"average_rate": item["words"] * 60 / item["elapsed_time"], "total_words": item["words"], "total_time": item["elapsed_time"], 
-                               "total_sessions": 1, "average_words_per_session": item["words"], "average_time_per_session": item["elapsed_time"]}]
+            #datetime_elapsed = datetime.fromisoformat(item["elapsed_time"])
 
-        with open(self.tracker, "a") as datafile:
+            #print("elapsed time: ", item["elapsed_time"])
+            #print(type(item["elapsed_time"]))
+            #elapsed_datetime = datetime.datetime.strptime(item["elapsed_time"], "%H:%M:%S")
+            #elapsed_time_timedelta = datetime.timedelta(hours=elapsed_datetime.hour, minutes=elapsed_datetime.minute, seconds=elapsed_datetime.second)
+            #elapsed_time = int(elapsed_time_timedelta.total_seconds())
+            #print(elapsed_time, " and changed type ", type(elapsed_time))
+
+            #now elapsed time is an integer, representing total elapsed seconds
+
+      
+            if item["date"] in data.keys():
+                #average rate is total words divided by total time, per hour
+
+                average_rate = (data[item]["total_words"] + item["words"] * 3600) / (data[item]["elapsed_time"] + item["elapsed_time_sec"])
+                data[item] = [{"average_rate": average_rate, "total_words": data[item]["total_words"] + item["words"], "total_time": data[item]["elapsed_time"] + item["elapsed_time_sec"],
+                               "total_sessions": data[item]["total_sessions"] + 1, "average_words_per_session": (data[item]["total_words"] + item["words"]) / (data[item]["total_sessions"] + 1),
+                               "average_time_per_session": (data[item]["elapsed_time"] + item["elapsed_time_sec"]) / (data[item]["total_sessions"] + 1)}]
+            else:
+                
+
+                data[item] = [{"average_rate": item["words"] * 60 / item["elapsed_time_sec"], "total_words": item["words"], "total_time": item["elapsed_time_sec"], 
+                               "total_sessions": 1, "average_words_per_session": item["words"], "average_time_per_session": time["elapsed_time_sec"]}]
+
+        with open(self.tracker, "w") as datafile:
             writer = csv.writer(datafile)
-            writer.writerow([])
+            writer.writerow(["Average Rate", "Total Words", "Total Time", "Total Sessions", "Average Words Per Session", "Average Time Per Session"])
+            for item in data:
+                writer.writerow([data[item]["average_rate"], data[item]["total_words"], data[item]["total_time"], data[item]["total_sessions"],
+                                 data[item]["average_words_per_session"], data[item]["average_time_per_session"]])
+                
+        #you have a file with all the sessions, all those sessions are being calculated into an {item, {data}} dictionary
+        #then, if that item is not in the file, then write it in
+        #load a number to be saved in the file to know what line to start on
 
     def load_session(self):
         # Loads all sessions for this project from a JSON file
@@ -124,7 +154,7 @@ class WritingSessionTracker:
             with open(self.jsonfile, 'r') as f:
                 return json.load(f)
         else:
-            return []
+            return [] #this is returning a list, it needs to return a dictionary!!
 
     def save_session(self):
         # Saves current writing session to a JSON file
