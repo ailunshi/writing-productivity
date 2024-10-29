4b671f27547eb4098075d339c61b90daca420758
ailunshi <ailunshi@berkeley.edu>
2024-10-29 13:52:17 -0700

    cleaned up code

--- writing.py
+++ writing.py
@@ -1,55 +1,20 @@
-#building a writing productivity tracker
+from writing_streamlined import WritingSessionTracker
 
-import datetime
-import csv
+class Writing:
+    def __init__(self, project):
+        self.project = project
+        self.project_file = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Bel e Kyre.scrivx"
+        self.data = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Files/Data"
+        self.memory = "sessions.json"
+        self.data_json = "data.json"
+        self.writing_tracker = "writingtracker.csv"
+        self.data_tracker = "data.csv"
 
+    def call_writing_streamlined(self):
+        new_session = WritingSessionTracker(self.project_file, self.data, self.memory, self.data_json, self.writing_tracker, self.data_tracker)
+        new_session.start_session()
 
-def str_time(time):
-    #Returns datetime object as a string
-    return time.strftime("%m/%d/%Y, %H:%M:%S")
-
-def chop_ms(delta):
-    #Returns timedelta object without microseconds
-    return delta - datetime.timedelta(microseconds=delta.microseconds)
-
-file_path = "new_file.csv"
-with open("new_file.csv", "a", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Start Time", "End Time", "Total Time"])
-
-
-response = input("Type anything to start.")
-if response == "":
-    print("No input was given.")
-else:
-    start_time = datetime.datetime.now()
-    str_start_time = str_time(start_time)
-    print("Start Time: ", str_start_time)
-
-response = input("Type anything once finished with your writing session.")
-if response == "":
-    print("No input was given.")
-else:
-    end_time = datetime.datetime.now()
-    str_end_time = str_time(end_time)
-    print("End Time: ", str_end_time)
-    total_time = chop_ms(end_time - start_time) #total_time is timedelta object
-    print("Total Time: ", str(total_time))
-
-    with open("new_file.csv", "a", newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([str_start_time, str_end_time, str(total_time)])
-
-
-"""
-steps:
-pull empty .txt file to reference
-use fake word counts for now?
-fill in calculations
-
-eventually:
-reference meta data from scrivener
-    not sure if it's possible to see if i've edited the page
-
-reference from google doc
-"""
+if __name__ == "__main__":
+    project = "Bel e Kyre"
+    writing_instance = Writing(project)
+    writing_instance.call_writing_streamlined()
