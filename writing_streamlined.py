1ef6551de8442e71409cd0baf6efab29f3b52748
ailunshi <ailunshi@berkeley.edu>
2024-10-29 13:52:40 -0700

    fixed dictionary bug, everything now works

--- writing_streamlined.py
+++ writing_streamlined.py
@@ -19,18 +19,33 @@ def unserialize_datetime(date_string):
         return datetime.datetime.fromisoformat(date_string)
     except ValueError:
         raise TypeError("Date string not in ISO format")
+    
+def save_datefiles(data):
+    with open("data.json", "w") as file:
+        json.dump(data, file, default=serialize_datetime)
+
+def load_session(self):
+        # Loads all sessions for this project from a JSON file
+
+        if os.path.exists(self.jsonfile):
+            with open(self.jsonfile, 'r') as f:
+                return json.load(f)
+        else:
+            return []
 
 class WritingSessionTracker:
-    def __init__(self, filename="sessions.json"):
+    def __init__(self, project, data, memory, data_json, writing_tracker, data_tracker):
         # Sets up a WritingSessionTracker object with sessions dictionary, tracker file, and data file
         
-        self.jsonfile = filename
-        self.all_sessions = self.load_session()
+        self.sessions_jsonfile = memory
+        self.data_jsonfile = data_json
+        self.all_sessions = self.load_session(self.sessions_jsonfile)
         self.session = {}
-        self.tracker = "writingtracker.csv"
-        self.tracker_data = "data.csv"
-        self.data = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Files/Data"
-        self.project_path = "/Users/balloon/Bel e Kyre/Bel e Kyre.scriv/Bel e Kyre.scrivx"
+        self.tracker = writing_tracker
+        self.tracker_data = data_tracker
+        self.all_data = self.load_session(self.data_jsonfile)
+        self.data = data
+        self.project_path = project
         self.session_dates = []
 
         for session in self.all_sessions:
@@ -77,7 +92,9 @@ class WritingSessionTracker:
             self.end_session(parser)
             self.write_to_raw_file()
             self.write_to_data_file()
-            self.save_session()
+
+            self.all_sessions.append(self.session)
+            self.save_session(self.all_sessions, self.sessions_jsonfile)
 
     def end_session(self, parser):
         # Ends session and calculates the data
@@ -101,11 +118,11 @@ class WritingSessionTracker:
     def write_to_data_file(self):
         # Compiles more useful data based on information from the CSV file
 
-        data = {}
 
         # Initialize data for the session date if it doesn't exist
         session_date = self.session["date"]
         if session_date not in self.session_dates:
+            data = {}
             data[session_date] = {
                 "average_rate": self.session["words"] / (self.session["elapsed_time_sec"] / 3600) if self.session["elapsed_time_sec"] > 0 else 0,
                 "total_words": self.session["words"],
@@ -114,12 +131,17 @@ class WritingSessionTracker:
                 "average_words_per_session": self.session["words"],
                 "average_time_per_session": self.session["elapsed_time_sec"]
             }
+
+            self.all_data.append(data)
         else:
             # Update data for the current session date
+            data = self.all_data[0]
+            print("total words prev ", data[session_date]["total_words"])
+
             data[session_date]["average_rate"] = (data[session_date]["total_words"] + self.session["words"]) / (data[session_date]["total_time"] + self.session["elapsed_time_sec"] / 3600)
-            data[session_date]["total_words"] += self.session["words"]
-            data[session_date]["total_time"] += self.session["elapsed_time_sec"] / 3600
-            data[session_date]["total_sessions"] += 1
+            data[session_date]["total_words"] = data[session_date]["total_words"] + self.session["words"]
+            data[session_date]["total_time"] = data[session_date]["total_time"] + self.session["elapsed_time_sec"] / 3600
+            data[session_date]["total_sessions"] = data[session_date]["total_sessions"] + 1
             data[session_date]["average_words_per_session"] = data[session_date]["total_words"] / data[session_date]["total_sessions"]
             data[session_date]["average_time_per_session"] = data[session_date]["total_time"] / data[session_date]["total_sessions"]
 
@@ -139,25 +161,21 @@ class WritingSessionTracker:
                     stats["average_words_per_session"],
                     stats["average_time_per_session"]
                 ])
+
+        #need to save data to memory
+        self.save_session(self.all_data, self.data_jsonfile)
   
-    def load_session(self):
+    def load_session(self, file):
         # Loads all sessions for this project from a JSON file
 
-        if os.path.exists(self.jsonfile):
-            with open(self.jsonfile, 'r') as f:
+        if os.path.exists(file):
+            with open(file, 'r') as f:
                 return json.load(f)
         else:
             return []
 
-    def save_session(self):
+    def save_session(self, file, jsonfile):
         # Saves current writing session to a JSON file
 
-        self.all_sessions.append(self.session)
-        with open("sessions.json", "w") as file:
-            json.dump(self.all_sessions, file, default=serialize_datetime)
-
-        print(file.name)
-
-if __name__== "__main__":
-    tracker = WritingSessionTracker()
-    tracker.start_session()
+        with open(jsonfile, "w") as f:
+            json.dump(file, f, default=serialize_datetime)
